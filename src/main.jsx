4df3f14a17675353a703b7e0afbd15dc8928d21d
Roman Zakrevskyi <romul3003@gmail.com>
2022-12-29 14:25:57 +0200

    Fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { worker } from "@uidotdev/react-query-api";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
